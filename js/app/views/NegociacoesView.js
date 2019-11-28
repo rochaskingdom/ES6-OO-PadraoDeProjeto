4feac88940f07ed418fdc11b51e0d1dc23c4299a
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-11-28 11:23:50 -0300

    Herança e reutilização de código

--- js/app/views/NegociacoesView.js
+++ js/app/views/NegociacoesView.js
@@ -1,8 +1,7 @@
-class NegociacoesView {
+class NegociacoesView extends View {
 
-    constructor(elemento) {
-
-        this._elemento = elemento;
+    constructor(elemento) { 
+        super(elemento);
     }
 
     _template(model) {
@@ -40,10 +39,4 @@ class NegociacoesView {
               
         `;
     }
-
-    update(model) {
-
-        this._elemento.innerHTML = this._template(model);
-    }
-
 }
