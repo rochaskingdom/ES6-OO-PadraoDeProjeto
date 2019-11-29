2f1df439339021eb2bd21dc2cdab8fbe7033e15e
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-11-29 10:05:10 -0300

    Aprendendo a trabalhar com o Proxy

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -7,12 +7,14 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         
-        this._listaNegociacoes = new ListaNegociacoes(model =>
-            this._negociacoesView.update(model));
-
+        /*
+        this._listaNegociacoes = 
+            new ListaNegociacoes(model => this._negociacoesView.update(model));
+        */
+       
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
-
+        
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#mensagemView'));
         this._mensagemView.update(this._mensagem);
