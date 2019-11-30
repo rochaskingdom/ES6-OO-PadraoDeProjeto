299e0820f43ee5812c72fa0a6ee903d071c93943
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-11-30 20:17:10 -0300

    Parâmetros REST

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -7,16 +7,15 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        this._listaNegociacoes = new Bind (
-            new ListaNegociacoes(),
-            this._negociacoesView($('#negociacoesView')),
-            'adiciona', 'esvazia');
-       
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(),
+            new NegociacoesView($('#negociacoesView')),
+            'adiciona', 'esvazia')
         
         this._mensagem = new Bind(
-            new Mensagem(), 
-            this._mensagemView,
+            new Mensagem(), new MensagemView($('#mensagemView')),
             'texto');
 
         this._mensagemView = new MensagemView($('#mensagemView'));
